95099e28cd3fc1ed1871a8ed0b2d8521f4ea4874
Ednah wairimu kimani <kimaniednah@gmail.com>
2025-12-03 10:51:34 +0300

    guides

---
+++ src/pages/Reviews.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Reviews() {
+  return (
+    <section>
+      <h2>Reviews</h2>
+      <p>Users can leave reviews on experience detail pages (future work).</p>
+    </section>
+  );
+}
